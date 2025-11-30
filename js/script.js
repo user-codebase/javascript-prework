5b897929c5b9e699b8493791be2c0a0ff2e196b2
Marcin Suder <marcin.code.github@gmail.com>
2025-11-30 16:01:33 +0100

    add varibales computerMove and playerMove

---
+++ js/script.js
@@ -0,0 +1,5 @@
+let computerMove = `kamień`;
+printMessage('Zagrałem ' + computerMove + '! Jeśli Twój ruch to papier, to wygrywasz!');
+
+let playerMove = 'papier';
+printMessage('Zagrałeś ' + playerMove + '!');
