4b91fe7ef75b1f94038b988f56eb930c7d944b2e
Marcin Suder <marcin.code.github@gmail.com>
2025-11-30 18:30:27 +0100

    add conditional logic for randomNumber

--- js/script.js
+++ js/script.js
@@ -1,5 +1,15 @@
-let computerMove = `kamień`;
-printMessage('Zagrałem ' + computerMove + '! Jeśli Twój ruch to papier, to wygrywasz!');
+let randomNumber = Math.floor(Math.random() * 3 + 1);
 
-let playerMove = 'papier';
-printMessage('Zagrałeś ' + playerMove + '!');
+console.log('Wylosowana liczba to: ' + randomNumber);
+
+let computerMove = 'nieznany ruch';
+
+if(randomNumber == 1){
+    computerMove = 'kamień';
+}else if(randomNumber == 2){
+    computerMove = 'papier';
+}else if(randomNumber == 3){
+    computerMove = 'nożyce';
+}
+
+printMessage('Mój ruch to: ' + computerMove);
